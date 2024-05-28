36ec7c8929ed4faf056c5b2b8ce1e80d03943f0a
Minjum <152857216+Minjum@users.noreply.github.com>
2024-05-28 15:22:10 +0200

    team buttons 2

---
+++ JavaScript/team.js
@@ -0,0 +1,3 @@
+document.getElementById('showButton').addEventListener('click',function() {    
+document.getElementById('displayText').classList.toggle('hidden');    
+document.getElementById('displayImage').classList.toggle('hidden'); });
