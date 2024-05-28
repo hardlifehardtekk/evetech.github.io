a8788dda50eb57631835211049314e5d52f58c5a
Amirm1378 <amir.mahdi.8731@gmail.com>
2024-05-28 10:23:46 +0200

    Kontakt script

---
+++ JavaScript/kontakt.js
@@ -0,0 +1,17 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    // Verhindert das Absenden des Formulars
+    event.preventDefault();
+
+    // Zugriff auf die Formularelemente
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    // Überprüft, ob alle Felder ausgefüllt sind
+    if (name === '' || email === '' || message === '') {
+        alert('Fehler, Sie müssen alle Felder ausfüllen.');
+    } else {
+        alert('Ihre Nachricht wurde gesendet. Es wird so schnell wie möglich beantworten.');
+        // Hier können Sie den Code hinzufügen, um die Nachricht tatsächlich zu senden
+    }
+});
