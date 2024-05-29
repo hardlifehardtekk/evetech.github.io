8dd2e39fd947c0d5850671f6c10d64243a58bce9
lkroener <lucaskroenert@mail.com>
2024-05-29 15:05:48 +0200

    removed script.js and team.js (redundant)

--- JavaScript/team.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('showButton').addEventListener('click',function() {    
-document.getElementById('displayText').classList.toggle('hidden');    
-document.getElementById('displayImage').classList.toggle('hidden'); });
